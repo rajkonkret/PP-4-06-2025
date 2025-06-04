ae60dcf090d63c67965594272a053f510caa0cf7
raajkonkret <rajkonkret660@gmail.com>
2025-06-04 15:39:30 +0200

    [1 22] update typy_danych_2_lista.py

--- typy_danych_3_krotka.py
+++ typy_danych_3_krotka.py
@@ -38,3 +38,36 @@ print(tupla4)  # ('Radek',)
 
 print(tupla_imiona.index("Radek"))  # index 0
 print(tupla_imiona.count("Radek"))  # występuje 1 raz
+
+# ropzakownie tupli
+tup = 1, 2
+print(type(tup))  # <class 'tuple'>
+
+a, b = 1, 2
+print(a, b)  # 1 2
+
+a, b = tup
+print(a, b)  # 1 2
+
+tup2 = 1, 2, 3
+# a, b = tup2  # ValueError: too many values to unpack (expected 2)
+a, *b = tup2  # * worek na pozostałe dane
+print(a, b)  # 1 [2, 3]
+
+tupla_imiona = ('Radek', "Tomek", "Zenek", "Ela")
+name1, name2, *name3 = tupla_imiona
+print(f"{name1=}, {name2=}, {name3=}")  # name1='Radek', name2='Tomek', name3=['Zenek', 'Ela']
+
+name1, *name2, name3 = tupla_imiona
+print(f"{name1=}, {name2=}, {name3=}")  # name1='Radek', name2=['Tomek', 'Zenek'], name3='Ela'
+
+*name1, name2, name3 = tupla_imiona
+print(f"{name1=}, {name2=}, {name3=}")  # name1=['Radek', 'Tomek'], name2='Zenek', name3='Ela'
+
+# zwróci posortowaną listę
+print(sorted(tupla_imiona))  # ['Ela', 'Radek', 'Tomek', 'Zenek']
+print(tupla_imiona)  # ('Radek', 'Tomek', 'Zenek', 'Ela') krotka się nie zmieni
+
+lista_z_tupli = list(tupla_imiona)
+print(lista_z_tupli)  # ['Radek', 'Tomek', 'Zenek', 'Ela']
+print(type(lista_z_tupli))  # <class 'list'>
