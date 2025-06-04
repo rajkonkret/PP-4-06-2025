4349ab1c438bae82d599d16e3998c6dee6908425
raajkonkret <rajkonkret660@gmail.com>
2025-06-04 12:27:53 +0200

    [1 6] update teksty.py

--- day_1/teksty.py
+++ day_1/teksty.py
@@ -26,3 +26,42 @@ print(encoded_s)  # b'Witaj \xc5\x9awiecie' - typ bajtowy
 # \xc5\x9a - zapis  w systemie szesnastkowym kodu literki Ś
 print(type(encoded_s))  # <class 'bytes'>
 print(encoded_s.decode('utf-8'))  # Witaj Świecie
+
+print(tekst)  # Witaj Świecie
+# Witaj Świecie
+# 01234567890... indeksy od 0
+
+print(tekst[4])  # "j" indeks 4, pozycja 5
+
+print(tekst.count("i"))  # występuje 3 razy
+print(tekst.count("j", 0, 4))  # wystapi 0 razy, indeksy 0123 bada
+
+imie = "Radek"
+print(imie)  # Radek
+
+print("Imie:", imie)  # Imie: Radek
+
+starszy = "Witaj %s!"  # %s - wstaw w to miejsce str
+print(starszy % imie)  # Witaj Radek!
+
+# f - string
+tekst_format = f"Mam na imię {imie} i lubię Python"
+print(tekst_format)  # Mam na imię Radek i lubię Python
+
+tekst_format = f"\tMam na imię {imie}\n i lubię Pythona.\b"
+print(tekst_format)
+# \t tabulator
+# \n - nowa linia
+# \b - backspace
+# "	Mam na imię Radek
+#  i lubię Pythona"
+
+print("Witaj {}!".format(imie))  # Witaj Radek!
+
+print("""Teskt
+    wielolinijkowy""")
+# "Teskt
+#     wielolinijkowy"
+
+"""Komentarz
+wielolinijkowy"""
