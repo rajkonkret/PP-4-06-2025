120632d42b0559461035c40adb0e52ea56065744
raajkonkret <rajkonkret660@gmail.com>
2025-06-04 11:01:07 +0200

    [1 3]add pierwszy.py

---
+++ day_1/pierwszy.py
@@ -0,0 +1,39 @@
+# PEP8
+# ctrl alt l - formatowanie kodu wg zasad PEP8
+print()  # wypisz/wydrukuj pustą linię
+
+print("Nazywam się Radek")  # Nazywam się Radek
+print("Nazywam się Radek")  # Nazywam się Radek
+print("Nazywam się Radek")  # Nazywam się Radek
+print("Nazywam się Radek")  # Nazywam się Radek
+print("Nazywam się Radek")  # Nazywam się Radek
+# ctrl d - powielanie linii
+print('Nazywam się Radek')  # Nazywam się Radek
+
+# print('Radek") # SyntaxError: unterminated string literal (detected at line 13)
+
+# type() - sprawdzenie typu danych
+print(type("Radek"))  # <class 'str'>, tekstowe, string
+
+print("39")
+print("39" + "39")  # konkatenacja 3939, łaczy stringi
+
+print(39)
+print(type(39))  # <class 'int'>, liczby calkowite
+print(39 + 39)  # 78
+
+# int() - rzutowanie na int
+print(int("39") + int("39"))  # 78
+
+# str() - rzutowanie na tekst
+print(str(39) + str(39))  # 3939
+
+# print("39" + 39)  # TypeError: can only concatenate str (not "int") to str
+print(int("39") + 39)  # 78
+
+print(168 * 35)  # 5880
+print("168" * 35)  # 5880
+# 168168168168168168168168168168168168168168168168168168168168168168168168168168168168168168168168168168168
+print(10 * "-")  # ----------
+
+
