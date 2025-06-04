119d3bbcc44f72cb9e121e8d2ad421af14c449d7
raajkonkret <rajkonkret660@gmail.com>
2025-06-04 14:41:11 +0200

    [1 17] add typy_danych_2_lista.py

--- day_1/typy_danych_2_lista.py
+++ day_1/typy_danych_2_lista.py
@@ -35,3 +35,32 @@ print(lista[len(lista) - 1])  # Anna
 print(lista[-1])  # Anna, ostatni element z listy
 print(lista[-4])  # Marek
 print(lista[-7])  # Radek
+
+# # ['Radek', 'Tomek', 'Zenek', 'Marek', 'Kuba', 'Ela', 'Anna']
+# slicowanie - fragment listy
+print(lista[0:3])  # ['Radek', 'Tomek', 'Zenek'] indeks 012
+print(lista[:3])  # ['Radek', 'Tomek', 'Zenek']
+print(lista[2:])  # ['Zenek', 'Marek', 'Kuba', 'Ela', 'Anna'], włącznie z ostatnim
+print(lista[2:6])  # ['Zenek', 'Marek', 'Kuba', 'Ela'] indeksy 2345, bez 6
+
+print(lista[2:15])  # ['Zenek', 'Marek', 'Kuba', 'Ela', 'Anna']
+print(lista[10:234])  # []
+
+print(lista[:])  # ['Radek', 'Tomek', 'Zenek', 'Marek', 'Kuba', 'Ela', 'Anna']
+print(lista[2:2])  # []
+print(lista[2:3])  # ['Zenek'], indeks 2
+print(lista[::2])  # ['Radek', 'Zenek', 'Kuba', 'Anna'] [start:stop:krok], co drugi element
+
+# ['Radek', 'Tomek', 'Zenek', 'Marek', 'Kuba', 'Ela', 'Anna']
+#     0        1        2        3        4      5       6
+#     -7       -6       -5      -4        -3     -2      -1
+
+print(lista[-2:0])  # [] -> [4:0]
+print(lista[0:-2])  # [0:5], ['Radek', 'Tomek', 'Zenek', 'Marek', 'Kuba']
+
+lista_15 = list(range(15))  # od 0 do 14
+print(lista_15)  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+print(lista_15[::2])  # [0, 2, 4, 6, 8, 10, 12, 14], krok=2
+
+print(lista[::-1])  # w odwrotnej kolejności
+# ['Anna', 'Ela', 'Kuba', 'Marek', 'Zenek', 'Tomek', 'Radek']
