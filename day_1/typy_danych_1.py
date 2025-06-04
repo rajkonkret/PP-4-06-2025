68340e34f6f540278fb79fd4e28cd00de286265a
raajkonkret <rajkonkret660@gmail.com>
2025-06-04 13:55:47 +0200

    [1 12] add typy_danych_1.py

---
+++ day_1/typy_danych_1.py
@@ -0,0 +1,22 @@
+wiek = 47  # int
+rok = 2025  # int
+temp = 36.6  # float
+
+print(wiek + rok)  # 2072
+print(wiek - rok)  # -1978
+print(wiek * rok)  # 95175
+print(wiek / rok)  # 0.023209876543209877 -> float
+
+print(rok // wiek)  # 43, część całkowita z dzielenia 2025 // 47 = 43 całe
+print(rok % wiek)  # modulo, reszta z dzielenia 2025 // 47 = 43 całe i reszty 4
+# 43 * 47 = 2021 + 4 = 2025
+print(10 % 3)  # 1, 3 całe i 1 reszty
+
+print(wiek ** rok)  # potęgowanie
+# len() długość sekwencji
+print(len(str(wiek ** rok)))  # 3386
+# print(len(str(wiek ** rok ** 2)))  # 3386
+# ValueError: Exceeds
+# the
+# limit(4300
+# digits) for integer string conversion; use sys.set_int_max_str_digits() to increase the limit
