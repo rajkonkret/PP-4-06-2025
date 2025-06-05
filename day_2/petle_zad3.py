ce5cb6a2c3b29f8056a174f1c68e632c5032a3fc
raajkonkret <rajkonkret660@gmail.com>
2025-06-05 14:44:16 +0200

    [2 27] update petle_zad3.py

--- day_2/petle_zad3.py
+++ day_2/petle_zad3.py
@@ -51,3 +51,14 @@ while True:
 
 print(lista)  # ['1', '2', '3', '4', '5', '6']
 print(lista_int)  # [1, 2, 3, 4, 5, 6]
+
+my_list = [1, 5, 2, 3, 5, 4, 5, 6, 5]
+while 5 in my_list:
+    my_list.remove(5)
+print(my_list)  # [1, 2, 3, 4, 6] usuniętea liczba 5, niezmieniona kolejnosć
+
+my_list = [1, 5, 2, 3, 5, 4, 5, 6, 5]
+print(dict.fromkeys(my_list))
+# {1: None, 5: None, 2: None, 3: None, 4: None, 6: None}
+unique_numbers = list(dict.fromkeys(my_list))
+print(unique_numbers)  # [1, 5, 2, 3, 4, 6] usnięte dulikaty bez z miany kolejności
