21622adffeaf8e55aacaa0047721a66334002722
raajkonkret <rajkonkret660@gmail.com>
2025-06-05 13:45:17 +0200

    [2 18] add petle_zad1.py

---
+++ day_2/petle_zad1.py
@@ -0,0 +1,65 @@
+# pętla - możliwośc wykonania kodu wielokrotnie
+# for - pętla iteracyjna
+import random
+
+for i in range(5):  # od 0 do 4
+    print(i)
+
+for i in range(5):
+    pass  # nic nie rób
+
+for _ in range(5):  # niema zmienna
+    print("Test podłoga")
+    # print(_)  # 4
+# Test podłoga
+# Test podłoga
+# Test podłoga
+# Test podłoga
+# Test podłoga
+
+for i in range(10):
+    print(i * 2)
+    print(i + 2)
+# 0
+# 2
+# 2
+# 3
+# 4
+
+#
+lista_kule = list(range(1, 50))  # od 1 do 49
+lista_wynik = []
+for _ in range(6):  # od 0 do 5
+    wyn = random.choice(lista_kule)
+    lista_kule.remove(wyn)
+    # print(wyn)
+    lista_wynik.append(wyn)
+
+print(lista_wynik)  # [27, 23, 8, 43, 14, 13]
+
+for i in range(10):
+    if i % 2 == 0:  # modulo
+        print(i, "parzysta")
+
+# 0 parzysta
+# 2 parzysta
+# 4 parzysta
+# 6 parzysta
+# 8 parzysta
+
+# list comprehensions
+lista3 = [j for j in range(10) if j % 2 == 0]
+print(lista3)  # [0, 2, 4, 6, 8]
+
+for c in lista3:  # pobiera po kolei wszystkie lementy z lista3
+    if c > 4:
+        print("Większe od 4")
+    elif c == 4:
+        print("Równe 4")
+    else:
+        print("Mniejsze od 4")
+# Mniejsze od 4
+# Mniejsze od 4
+# Równe 4
+# Większe od 4
+# Większe od 4
