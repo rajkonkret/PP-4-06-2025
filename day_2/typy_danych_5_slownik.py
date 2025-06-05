b3a00d3be3b240a71e910f9587271ad67a59e3f3
raajkonkret <rajkonkret660@gmail.com>
2025-06-05 10:51:10 +0200

    [2 6] update typy_danych_5_slownik.py

--- day_2/typy_danych_5_slownik.py
+++ day_2/typy_danych_5_slownik.py
@@ -42,8 +42,8 @@ dictionary.update({'data': '31-12-2025'})
 print(dictionary)  # {'imie': 'Radek', 'wiek': 67, 'data': '31-12-2025'}
 
 # # input() - pozwala wprowadzić dane do komputera
-tekst = input("Podaj imię")
-print(tekst)
+# tekst = input("Podaj imię") # zwraca str
+# print(tekst)
 # # Podaj imięRadek
 # # Radek
 # ctrl / - komentowanie
@@ -52,3 +52,28 @@ print(tekst)
 # pobrac dwie liczby
 # wyswietlic wynik obliczenia(+)
 # ctrl shift f - wyszuzkiwanie w plikach
+# a = int(input("Podaj pierwszą liczbę"))
+# b = int(input("Podaj drugą liczbę"))
+#
+# print(int(a) + float(b))
+
+# napisac program słownik pol - ang
+# stworzyc słownik
+# wyswietlic dostępne (klucze)
+# pobranie od użytkownika słowka do przetłumaczenia
+# wyswietlenie tłuamcznia (wartość dla klucza)
+pol_ang = {"kot": "cat", "pies": "dog", "dach": "roof"}
+print(f"znam takie słowa: {pol_ang.keys()}")
+odp = input("Podaj słówko do przetłuamczenia")
+# print(f"Tłumaczenie {odp}: {pol_ang[odp.strip().lower()]}")
+print(f"Tłuamczenie {odp}: {pol_ang.get(odp.strip().lower(), "Nie ma")}")
+# {'imie': 'Radek', 'wiek': 67, 'data': '31-12-2025'}
+# znam takie słowa: dict_keys(['kot', 'pies', 'dach'])
+# Podaj słówko do przetłuamczenia Zima
+# Tłuamczenie  Zima: Nie ma
+
+name1 = "GROSS"
+name2 = "groẞ"
+print(name1.lower() == name2.lower())  # False
+# """ Return a version of the string suitable for caseless comparisons. """
+print(name1.casefold() == name2.casefold())  # True
