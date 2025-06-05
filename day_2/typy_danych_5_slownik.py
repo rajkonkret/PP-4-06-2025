09cfd77f6f6a434e05f0c3829cad8ddd6bc72944
raajkonkret <rajkonkret660@gmail.com>
2025-06-05 10:05:57 +0200

    [2 4] add typy_danych_5_slownik.py

---
+++ day_2/typy_danych_5_slownik.py
@@ -0,0 +1,44 @@
+# słownik - para klucz-wartosc
+# {"user" : "Radek"}
+# klucze nie mogą sie powtarzac
+# odpowiednik jsona
+
+# pusty słownik
+dictionary = {}
+print(type(dictionary))  # <class 'dict'>
+print(dictionary)  # {}
+
+pusty_slownik = dict()
+print(type(pusty_slownik))  # <class 'dict'>
+print(pusty_slownik)  # {}
+
+# dodanie elementu do słownika
+dictionary['imie'] = "Radek"
+print(dictionary)
+print(dictionary)  # {'imie': 'Radek'}
+dictionary['wiek'] = 67
+print(dictionary)  # {'imie': 'Radek', 'wiek': 67}
+
+print(dictionary.keys())  # dict_keys(['imie', 'wiek'])
+print(dictionary.values())  # dict_values(['Radek', 67])
+print(dictionary.items())  # dict_items([('imie', 'Radek'), ('wiek', 67)])
+
+dict_list = {'imie': ["radek", "Tomek", "Zenek"]}
+print(dict_list)  # {'imie': ['radek', 'Tomek', 'Zenek']}
+
+# wypisanie wartosci
+print(dictionary['imie'])  # Radek
+print(dict_list['imie'])  # ['radek', 'Tomek', 'Zenek'] -> lista
+print(dict_list['imie'][2])  # Zenek
+
+# print(dictionary['Imie']) # KeyError: 'Imie'
+
+print(dictionary.get('imie'))  # Radek
+print(dictionary.get('Imie'))  # None
+print(dictionary.get('Imie', "default"))  # default
+print(dictionary.get('imie', "default"))  # Radek
+
+dictionary.update({'data': '31-12-2025'})
+print(dictionary)  # {'imie': 'Radek', 'wiek': 67, 'data': '31-12-2025'}
+
+
