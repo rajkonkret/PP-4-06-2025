461bb06e5e9f3f8e76ca143af2fd15f03b1c9d4f
raajkonkret <rajkonkret660@gmail.com>
2025-06-06 11:51:34 +0200

    [3 9] update start.py

--- day_3/start.py
+++ day_3/start.py
@@ -1,8 +1,13 @@
+# tworzymy pakiet (python package)
+# w pakiecie tworzymy plik fun.py
+# w tym pliku tworzymy funkcje powitanie()
 import pakiet
 
-# po dodaniu w __init__
+# po dodaniu w __init__ funkcji powitanie()
+# zmienna __all__ steruje widocznością elementów pakietu
 pakiet.powitanie()
 
+# dodajemy funkcje info()
 from pakiet import fun
 
 fun.info()  # Jestem pakietem
